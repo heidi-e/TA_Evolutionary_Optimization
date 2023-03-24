16bf14d13a641ce42a5cbc12532316312f2be252
heidi-e <100874463+heidi-e@users.noreply.github.com>
2023-03-23 23:51:31 -0400

    Changes by Conor

--- testing.py
+++ testing.py
@@ -4,11 +4,11 @@ import pandas as pd
 # read in data for global variables
 sections_df = pd.read_csv("sections.csv")
 tas_df = pd.read_csv("tas.csv")
-test1 = pd.read_csv("test1.csv", header = None)
+solutions = pd.read_csv("test1.csv", header = None)
 test2 = pd.read_csv("test2.csv", header = None)
 test3 = pd.read_csv("test3.csv", header = None)
 
-print(sections_df)
+#print(sections_df)
 """
 times = {}
 section_lst =[]
@@ -37,9 +37,57 @@ for sect_time, sections in section_lists.items():
 
 
 print(section_lists)"""
-list1 = [1, 2, 3, 4, 5]
+"""list1 = [1, 2, 3, 4, 5]
 list2 = [2, 4, 6, 8]
 
 intersection = set(list1).intersection(list2)
 
-print(intersection)
+print(intersection)"""
+""""""
+"""#test1.iloc[[0,:]]
+print(test1.iloc[[1]] == 1)
+
+print(tas_df.iloc[[0]] == 'U')
+
+#print(test1.iloc[[1]] == (tas_df.iloc[[0]] == 'U'))
+
+
+
+#print(tas_df[tas_df.iloc[[0]] == 'U'])
+
+print(test1.loc[(tas_df.iloc[[0]] == 'U')])
+
+def swapper(solutions):
+    #AGENT
+    Swap two random values
+    L = solutions[0]
+    i = rnd.randrange(0, len(L))
+    j = rnd.randrange(0, len(L))
+    L[i], L[j] = L[j], L[i]
+    return L
+
+"""
+
+print(solutions)
+for i in range(len(solutions)):
+    print(solutions.iloc[i])
+    num_allocated = solutions.iloc[i].sum()  # get total allocations for the ta
+    max_allocated = tas_df.iloc[i, 2]
+
+    if (num_allocated > max_allocated):
+        # this ta is overallocated
+
+        for col in solutions.columns:
+            # if the section is assigned
+            if solutions.iloc[i, col] == 1:
+                # if the section assigned is unwilling for the ta
+                if tas_df.iloc[i, (col + 3)] == 'U':
+                    solutions.iloc[i, col] = 0
+                    break
+
+
+
+
+
+print(solutions)
+
