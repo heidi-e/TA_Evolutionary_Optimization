537811bc6fe36dd5300662af2691748134077dff
heidi-e <100874463+heidi-e@users.noreply.github.com>
2023-03-23 23:50:48 -0400

    created agent and finished objectives

--- testing_agent.py
+++ testing_agent.py
@@ -5,33 +5,36 @@ import numpy as np
 # read in data for global variables
 sections_df = pd.read_csv("sections.csv")
 tas_df = pd.read_csv("tas.csv")
-test1 = pd.read_csv("test1.csv", header = None)
-test2 = pd.read_csv("test2.csv", header = None)
-test3 = pd.read_csv("test3.csv", header = None)
+test1 = pd.read_csv("test1.csv", header=None)
+test2 = pd.read_csv("test2.csv", header=None)
+test3 = pd.read_csv("test3.csv", header=None)
 
 
 def overallo(test):
-
     # sum across the row using numpy arrays
 
+    # number of times
     total_assigns = test.sum(axis=1)
+
+    # number of sections the TA prefers
     max_assigns = tas_df['max_assigned']
 
     difference = total_assigns.to_numpy() - max_assigns.to_numpy()
 
     return difference[difference > 0].sum()
 
-def time_conflict(test):
 
+def time_conflict(test):
     conflict = 0
     test = test.to_numpy()
 
     for ta in range(test.shape[0]):
-        times = sections_df.daytime[test[ta]==1]
+        times = sections_df.daytime[test[ta] == 1]
         if times.duplicated().sum() > 0:
             conflict += 1
 
     return conflict
+
 
 def under_supp(test):
     # sum across columns using numpy arrays
@@ -44,20 +47,21 @@ def under_supp(test):
 
     return difference[difference > 0].sum()
 
-def unwilling(test):
 
+def unwilling(test):
     conflict = 0
     test = test.to_numpy()
 
     # iterate through all TAs
     for ta in range(test.shape[0]):
         # find index of section that the TA was allocated to
-        for val in np.where(test[ta]==1):
+        for val in np.where(test[ta] == 1):
             # check if TA is unwilling to support that section index
             assign = tas_df.iloc[ta, val + 3] == 'U'
             conflict += assign.sum()
 
     return conflict
+
 
 def unpreferred(test):
     conflict = 0
@@ -70,8 +74,9 @@ def unpreferred(test):
 
     return conflict
 
+
 def swapper(solutions):
-    #AGENT
+    # AGENT
     """ Swap two random values """
     L = solutions[0]
     i = rnd.randrange(0, len(L))
@@ -79,57 +84,40 @@ def swapper(solutions):
     L[i], L[j] = L[j], L[i]
     return L
 
-def minimize_overallo(solutions):
-    #AGENT
-    """ yeet a TA from sections if they are overallocated and place them in a section that they are willing to be in"""
 
-    #solutions = pd.DataFrame(solutions)
 
-    # extract first test solution in solutions list
-    solution = solutions[len(solutions) - 1]
 
-    for i in range(len(solution)):
-        num_allocated = solution.iloc[i].sum()  # get total allocations for the ta
-        max_allocated = tas_df.iloc[i, 2]
-
-        if (num_allocated > max_allocated):
-            # this ta is overallocated
-
-            for col in solution.columns:
-                # if the section is assigned
-                if solution.iloc[i, col] == 1:
-                    # if the section assigned is unwilling for the ta
-                    if tas_df.iloc[i, (col + 3)] == 'U':
-                        solution.iloc[i, col] = 0
-                        break
-
-    return solution
-
-def swap_unwilling(test, ta_file):
-    #AGENT
-    # swap TAs that are overallocated based on their availability
-    #tah =
+def another_agent(test):
+    # AGENT
+    # swap TAs that are overallocated
 
     # sum across the row using numpy arrays
 
-    #for ta in
-    pass
+    # number of times
+    total_assigns = test.sum(axis=1)
+
+    # number of sections the TA prefers
+    max_assigns = tas_df['max_assigned']
+
+    difference = total_assigns.to_numpy() - max_assigns.to_numpy()
+
+    overallo_ta = np.where(difference > 0)
 
 
+    for ta in overallo_ta:
+        if difference[ta] == 2:
+            L = tas_df[ta] == 'U'
+            print(L)
+            """i = 0
+            j = 1
+            L[i], L[j] = L[j], L[i]"""
 
-    """for
-     
-        L = solutions[0]
-        i = rnd.randrange(0, len(L))
-        j = rnd.randrange(0, len(L))
-        L[i], L[j] = L[j], L[i]
-    
-    return L"""
 
 
 def main():
 
-    # Create framework
+    print(another_agent(test1))
+    """# Create framework
     E = Evo()
 
     # Register the five objectives
@@ -140,20 +128,15 @@ def main():
     E.add_fitness_criteria("unpreferred", unpreferred)
 
     # Register some agents
-    E.add_agent("minimize_overallo", minimize_overallo, k=1)
-    
+    E.add_agent("swapper", swap_overallo, k=1)
+
     # Seed the population with an initial random solution
     N = 50
 
     E.add_solution(test1)
-    #E.add_solution(test2)
-    #E.add_solution(test3)
-    # Run the evolver
-
-    E.run_agent("minimize_overallo")
-    #E.evolve(100000000, 100, 100000)
-
-    print(E)
+    # E.add_solution(test2)
+    # E.add_solution(test3)
+    print(E)"""
 
 
 if __name__ == '__main__':
